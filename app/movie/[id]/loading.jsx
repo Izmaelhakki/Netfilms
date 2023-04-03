58d76e16381b05fc002bf592c9c371dc05c2ed02
Izmaelhakki <ismailhakkiasmaz@gmail.com>
2023-04-03 16:36:16 +0300

    MovieDetail-Loading-Error

---
+++ app/movie/[id]/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MovieLoading() {
+  return (
+    <div style={{ height: "100%" }}>
+      <Loading />
+    </div>
+  );
+}
+
+export default MovieLoading;
--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import Genres from "@/mocks/genres.json";
 
 import { FeaturedMovie } from "@/components/featured-movie";
 import { Categories } from "@/components/categories";
+import { MoviesSection } from "@/components/movies-section";
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       <FeaturedMovie movie={Movies.results[0]} />
       <HomeContainer />
       <Categories categories={Genres.genres.slice(0,7)} />
+      <MoviesSection title="Popular Films" movies={Movies.results.slice(1,7)}/>
+      <MoviesSection title="Your Favurites" movies={Movies.results.slice(7,14)}/>
     </>
   );
 }
