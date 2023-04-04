6964eba6e07b15077417951dbb0f942a0758a14f
Izmaelhakki <ismailhakkiasmaz@gmail.com>
2023-04-04 11:13:27 +0300

    Detail and Loading Page -MockData Final

--- containers/home/index.jsx
+++ containers/home/index.jsx
@@ -7,11 +7,20 @@ import { FeaturedMovie } from "@/components/featured-movie";
 import { Categories } from "@/components/categories";
 import { MoviesSection } from "@/components/movies-section";
 
-export default function HomeContainer() {
+export default function HomeContainer({ selectedCategory }) {
   return (
     <>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 7)} />
+      {selectedCategory.movies.length > 0 && (
+        <MoviesSection
+          title={
+            Genres.genres.find((genre) => `${genre.id}` === selectedCategory.id)
+              .name
+          }
+          movies={selectedCategory.movies}
+        />
+      )}
       <MoviesSection
         title="Popular Films"
         movies={Movies.results.slice(1, 7)}
